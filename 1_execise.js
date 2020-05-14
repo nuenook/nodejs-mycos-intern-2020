ec9e81e4afb8ae80604c5195fd2b1b788eb732d0
unknown <flukbboydow@gmail.com>
2020-05-14 17:01:06 +0700

    Done on 1_execise

--- 1_execise.js
+++ 1_execise.js
@@ -2,8 +2,11 @@
  * Load data.json 
  * const data = data.json
  */
+const data =require("./data.json");
 
 
+const petStd =data.students.map(std=> std.pets)
+console.log(petStd);
  /**
   * get All pets
   * result pets:  [{
@@ -18,18 +21,58 @@
 
 
 
+const numPet= data.students.map(pet=>{
+  let numOfPet = pet.pets.length;
+  let studentName =pet.studentName;
+  return {studentName,numOfPet}
+})
+
+console.log(numPet);
 /**
  * count of each student's pet
  * show in result: [{studentName: "A", numOfPet: 2}, {studentName: "B", numOfPet: 1}]
  */
 
 
+ const imgStd = data.students.map(std=>{
+  let studentName =std.studentName;
+  let imageOj = data.imageOfStudent.find(img=>img.studentId==std.studentId)
+  let image = imageOj.imgPath
+  return {studentName,image}
+ })
+console.log(imgStd)
+
  /**
  * get image of student 
  * result: [{studentName: "A" , image:"public/img/student1.jpg"}]
  */
+console.log("-----------------------------")
+ const addPetType = data.students.map(All=>{
+    let petTypeOb = {"petTypeId":0,"petTypeName":"dog","petTypeId":1,"petTypeName":"cat",} 
+   
+    console.log({...All})
+    console.log("--------------------------------------------------------")
 
- 
+    All.pets=All.pets.map(pet=>{
+        let petType = 0;
+        let petName =pet.petName;
+        if(pet.petType == "cat") petType = 1;
+      return {petName,petType};
+    })
+
+   return {...All} 
+
+
+ })
+
+ let petTypeOb = {"petTypeOb":[
+   {"petTypeId":0,"petTypeName":"dog"},
+   {"petTypeId":1,"petTypeName":"cat"}
+]}
+
+addPetType.push(petTypeOb)
+console.log(addPetType);
+
  /**
   * add petType object on top level {petTypeId: 0, petTypeName: "dog"}
   * convert petType to 0 is dog and 1 is cat
